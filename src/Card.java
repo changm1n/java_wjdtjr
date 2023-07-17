d458c5422f348421ac60a10e60490c9ef17aaa2e
changm1n <leo4109@gmail.com>
2023-07-17 22:19:42 +0900

    second commit

---
+++ src/Card.java
@@ -0,0 +1,21 @@
+// 멤버변수와 멤버변수를 제외한 변수(지역변수)로 나눌수 있다.
+// static이 붙은면 클래스변수, 붙지 않으면 인스턴스 변수이다.
+class Varibles {
+    int iv; // 인스턴스 변수
+    static int cv; // 클래스 변수
+
+    void method()
+    {
+        int lv = 0;  // 지역변수
+    }
+}
+
+//클래스 변수와 인스턴스변수의 차이
+public class Card {
+    String kind;
+    int number;
+
+    static int width = 100;
+    static int height = 250;
+}
+//카드를 예시로 설명하면 각 카드 인스턴스는 자신만의 무늬와 숫자를 가지고 있지만 모든 카드 인스턴스는 높이와 너비가 같다.//
